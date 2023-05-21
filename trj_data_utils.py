61aa9a0c29e5d81065d124088251f31e5ebe634c
Vickers Zhu <vickerszhu@163.com>
2023-05-22 00:41:21 +0900

    Step 2: Deleted useless snippets

--- trj_data_utils.py
+++ trj_data_utils.py
@@ -29,39 +29,3 @@ def read_trj_data(filename):
             else:
                 trajectory.append([frame,xpos,ypos,zpos,xvel,yvel,zvel])
     return data
-
-# from TrjSample import TrjSample
-
-# def read_trj_fulldata(filename):
-#     trj_samples = []  # Initialize an empty list to store TrjSample objects
-#     # Read the data from the file and create TrjSample objects
-#     with open(filename, "r") as file:
-#         for line in file:
-#             values = line.split()
-#             if len(values) != 19:
-#                 print('continue')
-#                 continue
-#             trj_sample = TrjSample.TrjSample(
-#                 int(values[0]),
-#                 int(values[1]),
-#                 int(values[2]),
-#                 float(values[3]),
-#                 float(values[4]),
-#                 float(values[5]),
-#                 float(values[6]),
-#                 float(values[7]),
-#                 float(values[8]),
-#                 float(values[9]),
-#                 float(values[10]),
-#                 float(values[11]),
-#                 float(values[12]),
-#                 float(values[13]),
-#                 float(values[14]),
-#                 int(values[15]),
-#                 float(values[16]),
-#                 float(values[17]),
-#                 float(values[18])
-#             )
-#             trj_samples.append(trj_sample)
-    
-#     return trj_samples
