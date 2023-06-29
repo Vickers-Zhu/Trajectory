d39289803e1bef28db06c8b9f3d762628b3b0fb9
Vickers Zhu <vickerszhu@163.com>
2023-06-30 02:59:38 +0900

    Added all trajs

--- trj_data_utils.py
+++ trj_data_utils.py
@@ -1,5 +1,7 @@
 # import TrjSample
 import matplotlib.pyplot as plt
+import numpy as np
+from correlation_delay import calculate_correlation_pairs_multiple_tau, get_traj
 
 def read_trj_data(filename):
     with open(filename,mode='r',encoding='utf-8') as file:
@@ -29,3 +31,34 @@ def read_trj_data(filename):
             else:
                 trajectory.append([trj_id,frame,xpos,ypos,zpos,xvel,yvel,zvel])
     return data
+
+def calculate_heatmap_data(data, bird_ids, tau_values):
+    num_birds = len(bird_ids)
+    heatmap_data = np.zeros((num_birds, num_birds))
+    tau_max_data = np.zeros((num_birds, num_birds))
+
+    # Calculate the maximum Cij(tau) values and corresponding tau values for each pair of birds
+    for i, bird_i in enumerate(bird_ids):
+        for j, bird_j in enumerate(bird_ids[i + 1:], start=i + 1):
+            correlation_pairs = calculate_correlation_pairs_multiple_tau(data, bird_i, bird_j, tau_values)
+            c_ij_values = correlation_pairs[(bird_i, bird_j)]
+            max_c_ij_tau = max(c_ij_values)
+            max_tau_index = c_ij_values.index(max_c_ij_tau)
+            max_tau = tau_values[max_tau_index]
+            heatmap_data[i, j] = max_c_ij_tau
+            heatmap_data[j, i] = max_c_ij_tau  # Fill in the symmetric entry
+            tau_max_data[i, j] = max_tau
+            tau_max_data[j, i] = max_tau  # Fill in the symmetric entry
+
+    return heatmap_data, tau_max_data
+
+def find_trajectory_ids(trajectories, length_threshold):
+    selected_ids = []
+    for n in range(120):
+        try:
+            traj = get_traj(trajectories, n)
+            if len(traj) > length_threshold:
+                selected_ids.append(n)
+        except ValueError:
+            pass
+    return selected_ids
