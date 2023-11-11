a7503e2a83f2775b4b1cc50f17324c24439742d7
Vickers Zhu <vickerszhu@163.com>
2023-11-12 00:34:22 +0900

    Refact work dir

---
+++ scripts/cordelay/correlation_delay.py
@@ -0,0 +1,80 @@
+import numpy as np
+
+
+def calculate_directional_correlation_delay(trajectories, bird_i, bird_j, tau):
+    if tau < 0:
+        bird_i, bird_j = bird_j, bird_i
+        tau = -tau
+    # for traj in trajectories:
+    #     print(traj[0][0])
+    trajectory_i = get_traj(trajectories, bird_i)
+    trajectory_j = get_traj(trajectories, bird_j)
+
+    frames_i, velocities_i = np.array(trajectory_i)[:, 1], np.array(
+        trajectory_i)[:, 5:8]  # Extract frames and velocities from trajectory_i
+    frames_j, velocities_j = np.array(trajectory_j)[:, 1], np.array(
+        trajectory_j)[:, 5:8]  # Extract frames and velocities from trajectory_j
+
+    # Find the common frames between trajectory_i and trajectory_j
+    common_frames = np.intersect1d(frames_i, frames_j, assume_unique=True)
+
+    # Select velocities_i for common frames
+    velocities_i_common = velocities_i[np.isin(frames_i, common_frames)]
+    # Select velocities_j for common frames
+    velocities_j_common = velocities_j[np.isin(frames_j, common_frames)]
+
+    if tau > len(velocities_i_common):
+        raise ValueError(
+            'tau is larger than the number of points in the trajectory')
+
+    # Shift velocities_j_common by tau frames
+    velocities_j_delayed = velocities_j_common[tau:]
+
+    # Compute the numerator
+    numerator = np.sum(
+        velocities_i_common[: -tau if tau != 0 else None] * velocities_j_delayed)
+    denominator = np.linalg.norm(velocities_i_common[: -tau if tau != 0 else None]) * np.linalg.norm(
+        velocities_j_delayed)  # Compute the denominator
+
+    c_ij_tau = numerator / denominator  # Calculate Cij(τ)
+
+    return c_ij_tau
+
+
+def calculate_correlation_pairs_multiple_tau(trajectories, bird_i, bird_j, tau_values):
+    correlation_pairs = {}  # Dictionary to store the correlation values for each pair
+    c_ij_values = []  # List to store the c_ij_tau values
+
+    for tau in tau_values:
+        if len(get_traj(trajectories, bird_i)) < 60 or len(get_traj(trajectories, bird_j)) < 60:
+            print('Skipped. Too few points in trajectories.')
+            continue
+        c_ij_tau = calculate_directional_correlation_delay(
+            trajectories, bird_i, bird_j, tau)
+        c_ij_values.append(c_ij_tau)
+
+    pair_key = (bird_i, bird_j)
+    correlation_pairs[pair_key] = c_ij_values
+    return correlation_pairs
+
+
+def calculate_normalized_velocity(velocity):
+    norm = np.linalg.norm(velocity)
+    if norm == 0:
+        return velocity
+    return velocity / norm
+
+
+def get_traj(trajectories, n):
+    for traj in trajectories:
+        if len(traj) > 0 and len(traj[0]) > 0 and traj[0][0] == n:
+            return traj
+    raise ValueError(f"No trajectory found with trj_id = {n}")
+
+
+def get_trajs_by_ids(trajectories, bird_ids):
+    selected_trajectories = []
+    for traj in trajectories:
+        if len(traj) > 0 and len(traj[0]) > 0 and traj[0][0] in bird_ids:
+            selected_trajectories.append(traj)
+    return selected_trajectories
---
+++ scripts/trj_data_utils.py
@@ -0,0 +1,119 @@
+# import TrjSample
+import matplotlib.pyplot as plt
+import numpy as np
+from scripts.cordelay.correlation_delay import calculate_correlation_pairs_multiple_tau, get_traj
+
+
+def read_trj_data(filename):
+    with open(filename, mode='r', encoding='utf-8') as file:
+        lines = file.readlines()
+    data = []
+    cnt = 0
+    trj_prev = -1
+    for line in lines:
+        cols = line.split()
+        if len(cols) > 1:  # skip empty line
+            trj_id = int(cols[0])
+            frame = int(cols[1])
+            xpos = float(cols[6])  # depth
+            ypos = float(cols[7])
+            zpos = float(cols[8])  # height
+            xvel = float(cols[12])
+            yvel = float(cols[13])
+            zvel = float(cols[14])
+            n = int(cols[15])  # length of sequence
+
+            if trj_prev != trj_id:
+                if trj_prev != -1:  # very first one
+                    data.append(trajectory)
+                trajectory = []
+                trj_prev = trj_id
+                cnt += 1
+            else:
+                trajectory.append(
+                    [trj_id, frame, xpos, ypos, zpos, xvel, yvel, zvel])
+    return data
+
+
+def calculate_heatmap_data(data, bird_ids, tau_values):
+    num_birds = len(bird_ids)
+    heatmap_data = np.zeros((num_birds, num_birds))
+    tau_max_data = np.zeros((num_birds, num_birds))
+
+    # Calculate the maximum Cij(tau) values and corresponding tau values for each pair of birds
+    for i, bird_i in enumerate(bird_ids):
+        for j, bird_j in enumerate(bird_ids[i + 1:], start=i + 1):
+            correlation_pairs = calculate_correlation_pairs_multiple_tau(
+                data, bird_i, bird_j, tau_values)
+            c_ij_values = correlation_pairs[(bird_i, bird_j)]
+            max_c_ij_tau = max(c_ij_values)
+            max_tau_index = c_ij_values.index(max_c_ij_tau)
+            max_tau = tau_values[max_tau_index]
+            heatmap_data[i, j] = max_c_ij_tau
+            heatmap_data[j, i] = max_c_ij_tau  # Fill in the symmetric entry
+            tau_max_data[i, j] = max_tau
+            tau_max_data[j, i] = max_tau  # Fill in the symmetric entry
+
+    return heatmap_data, tau_max_data
+
+
+def create_tau_relations_matrix(heatmap_data, tau_max_data, bird_ids):
+    tau_relations = np.zeros((len(bird_ids), len(bird_ids)))
+
+    for i, bird_i in enumerate(bird_ids):
+        for j, bird_j in enumerate(bird_ids[i + 1:], start=i + 1):
+            if tau_max_data[i, j] < 0:
+                tau_relations[j, i] = heatmap_data[i, j]
+            else:
+                tau_relations[i, j] = heatmap_data[i, j]
+
+    return tau_relations
+
+
+def find_related_most(tau_relations, bird_ids):
+    related_most = {}
+
+    for i, bird_i in enumerate(bird_ids):
+        max_index = np.argmax(tau_relations[i, :])
+        print('index i: ', i, 'Max index, ', max_index)
+        print(tau_relations[i, :])
+        related_bird_id = bird_ids[max_index]
+        related_most[bird_i] = related_bird_id
+
+    return related_most
+
+
+def find_trajectory_ids(trajectories, length_threshold):
+    selected_ids = []
+    for n in range(120):
+        try:
+            traj = get_traj(trajectories, n)
+            if len(traj) > length_threshold:
+                selected_ids.append(n)
+        except ValueError:
+            pass
+    return selected_ids
+
+
+def calculate_acceleration(selected_trajectories):
+    acceleration_data = []
+
+    for trajectory in selected_trajectories:
+        acceleration_each = []
+        for i in range(len(trajectory) - 1):
+            trj_id = trajectory[i][0]
+            frame = trajectory[i][1]
+            xvel = trajectory[i][5]
+            yvel = trajectory[i][6]
+            zvel = trajectory[i][7]
+            next_xvel = trajectory[i+1][5]
+            next_yvel = trajectory[i+1][6]
+            next_zvel = trajectory[i+1][7]
+            xacc = next_xvel - xvel
+            yacc = next_yvel - yvel
+            zacc = next_zvel - zvel
+
+            acceleration_each.append([trj_id, frame, xacc, yacc, zacc])
+        acceleration_data.append(acceleration_each)
+
+    return acceleration_data
